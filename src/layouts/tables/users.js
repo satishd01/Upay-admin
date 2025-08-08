d7fd739ff257c41aa3779f9cadcb30c4ada64a09
satish donerao <85147362+satishd01@users.noreply.github.com>
2025-08-08 13:12:34 +0530

    Update users.js

--- src/layouts/tables/users.js
+++ src/layouts/tables/users.js
@@ -27,7 +27,7 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import DataTable from "examples/Tables/DataTable";
 
-const BASE_URL = "http://54.146.70.141:8090";
+const BASE_URL = "https://egrecharge.shellcode.website";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
